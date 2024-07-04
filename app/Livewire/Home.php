9db7918a99191dec2150969c6f614a8435048cad
kamal-abdul-khalik <elsakcf@gmail.com>
2024-07-05 02:32:53 +0800

    add stats to home

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,14 +2,44 @@
 
 namespace App\Livewire;
 
+use App\Models\Transaction;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+use Masmerise\Toaster\Toastable;
 
 #[Title('Home')]
 class Home extends Component
 {
+    use WithPagination, WithoutUrlPagination;
+    use Toastable;
+
+    public function toogleDone(Transaction $transaction)
+    {
+        $transaction->is_done = !$transaction->is_done;
+        $transaction->save();
+        $this->success('Trasaksi Selesai');
+    }
+
     public function render()
     {
-        return view('livewire.home');
+        $today = date('Y-m-d');
+        [$year, $month] = explode("-", date('Y-m'));
+
+        $income = Transaction::query()
+            ->whereMonth('created_at', $month)
+            ->whereYear('created_at', $year);
+
+        $transactions = Transaction::query()
+            ->where('is_done', false)
+            ->with('customer')
+            ->paginate(10);
+
+        return view('livewire.home', [
+            'monthly' => $income->get()->sum('total'),
+            'today' => $income->whereDate('created_at', $today)->get(),
+            'transactions' => $transactions,
+        ]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,78 @@
-<div>
-    Home: Because she competes with no one, no one can compete with her.
+<div class="page-wrapper">
+    <div class="grid grid-cols-3 gap-6">
+        <div class="card card-compact">
+            <div class="card-body">
+                <div class="flex items-center gap-x-3">
+                    <div class="p-3 rounded-full bg-primary"><x-tabler-calendar-month /></div>
+                    <div class="flex flex-col">
+                        <div class="font-semibold opacity-50">Pendapatan Bulan Ini</div>
+                        <div class="text-xl font-semibold">Rp. {{ Number::format($monthly, locale: 'id') }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card card-compact">
+            <div class="card-body">
+                <div class="flex items-center gap-x-3">
+                    <div class="p-3 rounded-full bg-primary"><x-tabler-hours-24 /></div>
+                    <div class="flex flex-col">
+                        <div class="font-semibold opacity-50">Pendapatan Hari Ini</div>
+                        <div class="text-xl font-semibold">
+                            Rp. {{ Number::format($today->sum('total'), locale: 'id') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card card-compact">
+            <div class="card-body">
+                <div class="flex items-center gap-x-3">
+                    <div class="p-3 rounded-full bg-primary"><x-tabler-shopping-cart-star /></div>
+                    <div class="flex flex-col">
+                        <div class="font-semibold opacity-50">Transaksi Hari ini</div>
+                        <div class="text-xl font-semibold">{{ $today->count() }} Transaksi</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="table-wrapper">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Tanggal Order</th>
+                    <th>Nama Pelanggan</th>
+                    <th>Total Bayar </th>
+                    <th>Keterangan</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($transactions as $item)
+                    <tr wire:key="{{ $item->id }}">
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->created_at->format('d M Y') }}</td>
+                        <td>{{ $item->customer?->name }}</td>
+                        <td>Rp. {{ Number::format($item->total, locale: 'id') }}</td>
+                        <td>{{ $item->desc }}</td>
+                        <td>
+                            <input type="checkbox" class="toggle toggle-xs" @checked($item->is_done)
+                                wire:change="toogleDone({{ $item->id }})" />
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="6" class="text-lg font-semibold text-center opacity-50">Belum ada transaksi
+                            disini
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    <div>
+        {{ $transactions->links() }}
+    </div>
 </div>
